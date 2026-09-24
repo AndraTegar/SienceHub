a9cae2eac7b9080ef79034889160db84781d10f0
RIDH0517 <rakaraya127@gmail.com>
2026-09-24 17:56:43 +0700

    Ridho Ganteng

--- app/src/main/java/com/kelompoksix/siencehub/MainActivity.kt
+++ app/src/main/java/com/kelompoksix/siencehub/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SienceHubTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Ridho GANTENG",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
