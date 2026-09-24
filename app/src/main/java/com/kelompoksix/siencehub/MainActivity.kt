563f6ee9c5f159f09aeebc81fb73f903792dbab9
RIDH0517 <rakaraya127@gmail.com>
2026-09-24 20:50:02 +0700

    Ridho Gantengg

--- app/src/main/java/com/kelompoksix/siencehub/MainActivity.kt
+++ app/src/main/java/com/kelompoksix/siencehub/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.kelompoksix.siencehub.ui.screens.BerandaScreen
 import com.kelompoksix.siencehub.ui.theme.SienceHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SienceHubTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Ridho Gantengg",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                BerandaScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SienceHubTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/kelompoksix/siencehub/ui/screens/BerandaScreen.kt
+++ app/src/main/java/com/kelompoksix/siencehub/ui/screens/BerandaScreen.kt
@@ -1,2 +1,9 @@
 package com.kelompoksix.siencehub.ui.screens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BerandaScreen() {
+    Text(text = "HALOO SAYA RIDHO")
+}
